5afaa614db0ec4f1aee475db7bfa65d250126ff0
AshleighAviles <168010696+AshleighAviles@users.noreply.github.com>
2024-11-25 09:22:29 -0500

    updated api path and box path with michael

---
+++ install packages.R
@@ -0,0 +1,12 @@
+
+#Install these packages for Michael's code. 
+install.packages("REDCapR")
+install.packages("tidyverse")
+install.packages("targets")
+install.packages("flextable")
+install.packages("officer")
+install.packages("officedown")
+install.packages("future")
+install.packages("future.callr")
+install.packages("future.batchtools")
+            
