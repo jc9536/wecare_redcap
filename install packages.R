0130ffc254c0793a158a062a023838e694f99ee0
Jaimie Chin <jc9536@nyu.edu>
2025-05-14 15:11:05 -0700

    update package installation

--- install packages.R
+++ install packages.R
@@ -3,10 +3,12 @@
 install.packages("REDCapR")
 install.packages("tidyverse")
 install.packages("targets")
+install.packages("tarchetypes")
 install.packages("flextable")
 install.packages("officer")
 install.packages("officedown")
 install.packages("future")
 install.packages("future.callr")
 install.packages("future.batchtools")
-            
+install.packages("here")
+install.packages("janitor")
